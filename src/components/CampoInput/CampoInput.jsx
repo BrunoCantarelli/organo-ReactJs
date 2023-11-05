df77bf36903d9e5bb24bc55d136f47acbc553cb5
Bruno Cantarelli <cantarellibruno2004@gmail.com>
2023-11-05 18:05:14 -0300

    att 5/11/23

--- src/components/CampoInput/CampoInput.jsx
+++ src/components/CampoInput/CampoInput.jsx
@@ -1,14 +1,22 @@
 /* eslint-disable react/prop-types */
 import './CampoInput.css'
 
+
 const CampoInput = (props) => {
+
+    const placeholderModificada = `${props.placeholder}...`
+
+    let valor = 'Bruno Cantarelli'
+
+    const aoDigitado = (evento) => {
+        console.log(evento.target.value)
+    }
 
     return (
         <div className='input-container'>
             <label>{props.label}</label>
-            <input required={props.obrigatorio} placeholder={props.placeholder}/>
+            <input value={valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={placeholderModificada}/>
         </div>
     )
 }
-
 export default CampoInput
